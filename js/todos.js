a021a7d64e5eba8726f915a046f390f4f43760ef
Chris Keene <ctkeene@gmail.com>
2014-06-16 21:12:34 -0700

    Update todos.js

--- js/todos.js
+++ js/todos.js
@@ -7,8 +7,8 @@ $(function() {
   Parse.$ = jQuery;
 
   // Initialize Parse with your Parse application javascript keys
-  Parse.initialize("your-application-id",
-                   "your-javascript-key");
+  Parse.initialize("WoTbsVPDNTe42GnvEYm2eIShdEdn8Y7HlilL6gAj",
+                   "ycCAlKhAplYPAi5TJhCwfgYOklYbe7LiGaeWojtB");
 
   // Todo Model
   // ----------
